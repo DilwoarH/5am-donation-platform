77e5c5c40d5d01b0d6b8d78c07d96aa5123505e3
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-03-03 02:49:43 +0000

    Adds one off donations

--- js/index.js
+++ js/index.js
@@ -34,3 +34,44 @@ checkoutButton.addEventListener('click', function () {
     amount.classList.add('govuk-input--error');
   }
 });
+
+// ONE OFF PAYMENT
+
+var oneOff_checkoutButton = document.getElementById('checkout-button-sku_Gq6VFrlK5gP4sc');
+var oneOff_amount = document.getElementById('one-off-amount').value;
+oneOff_checkoutButton.addEventListener('click', function () {
+  oneOff_checkoutButton.disabled = true;
+  // When the customer clicks on the button, redirect
+  // them to Checkout.
+
+  try {
+    stripe.redirectToCheckout({
+      items: [{sku: 'sku_Gq6VFrlK5gP4sc', quantity: parseInt(oneOff_amount)}],
+
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://5am.netlify.com/success.html',
+      cancelUrl: 'https://5am.netlify.com/cancelled.html',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayErrorWrapper = document.getElementById('one-off-error-message-wrapper');
+        var displayError = document.getElementById('one-off-error-message');
+        displayError.textContent = result.error.message;
+        displayErrorWrapper.style.display = 'block';
+      }
+    });
+
+  } catch (error) {
+    var displayErrorWrapper = document.getElementById('one-off-error-message-wrapper');
+    var displayError = document.getElementById('one-off-error-message');
+    displayError.textContent = error.message;
+    oneOff_amount.classList.add('govuk-input--error');
+    displayErrorWrapper.style.display = 'block';
+  }
+});
